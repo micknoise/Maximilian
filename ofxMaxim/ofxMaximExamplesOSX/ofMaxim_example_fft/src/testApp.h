cade501837381117c7e4f876fe32ce86920f79d3
Mick Grierson <mickgrierson@strangebook.local>
2011-08-23 18:23:16 +0100

    added ofxMaxim

---
+++ ofxMaxim/ofxMaximExamplesOSX/ofMaxim_example_fft/src/testApp.h
@@ -0,0 +1,62 @@
+#ifndef _TEST_APP
+#define _TEST_APP
+
+
+#include "ofMain.h"
+#include "ofxMaxim.h"
+#include <sys/time.h>
+
+class testApp : public ofBaseApp{
+
+	public:
+		~testApp();/* deconsructor is very useful */
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+	
+	void audioRequested 	(float * input, int bufferSize, int nChannels); /* output method */
+	void audioReceived 	(float * input, int bufferSize, int nChannels); /* input method */
+	
+	float 	* lAudioOut; /* outputs */
+	float   * rAudioOut;
+
+	float * lAudioIn; /* inputs */
+	float * rAudioIn;
+	
+	int		initialBufferSize; /* buffer size */ 
+	int		sampleRate;
+	
+	
+	/* stick you maximilian stuff below */
+	
+	double wave,sample,outputs[2], ifftVal;
+	maxiMix mymix;
+	maxiOsc osc;
+	
+	ofxMaxiFFTOctaveAnalyzer oct;
+	int nAverages;
+	float *ifftOutput;
+	int ifftSize;
+	
+	ofxMaxiIFFT ifft;
+	ofxMaxiFFT mfft;
+	int fftSize;
+	int bins, dataSize;
+	
+	float callTime;
+	timeval callTS, callEndTS;
+
+	
+	
+	
+};
+
+#endif

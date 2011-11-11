d7aa9b58c757d7da91adb907f9b0262ac2815eaf
Mick Grierson <mickgrierson@gmail.com>
2011-11-11 17:28:53 +0000

    tiny changes

--- main.cpp
+++ main.cpp
@@ -47,10 +47,10 @@ void play(double *output) {//this is where the magic happens. Very slow magic.
 		lastCount=0;//set lastCount to 0
 	}
 	
-	bassout=filter2.lores(envelope.adsr(bass.saw(currentPitch*0.5)+sound.pulse(currentPitch*0.5,mod.phasor(1)),1,0.9995, 0.25, 0.9995, 1, trigger),9250,2);//new, simple ADSR. 
+//	bassout=filter2.lores(envelope.adsr(bass.saw(currentPitch*0.5)+sound.pulse(currentPitch*0.5,mod.phasor(1)),1,0.9995, 0.25, 0.9995, 1, trigger),9250,2);//new, simple ADSR. 
 	leadout=filter.lores(leadenvelope.ar(lead2.saw(leadPitch*4)+lead.pulse(leadPitch+(leadmod.sinebuf(1.9)*1.5), 0.6), 0.00005, 0.999975, 50000, trigger2),5900,10);//leadline
 	
-	delayout=(leadout+(delay.dl(leadout, 14000, 0.8)*0.5))/2;//add some delay
+	delayout=(leadout+(delay.dl(leadout, 14000, 0.8)*0.5))/4;//add some delay
 	
 	if(trigger!=0)trigger=0;//set the trigger to off if you want it to trigger immediately next time.
 	

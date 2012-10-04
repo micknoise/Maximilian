b3a86a84e18bafd5b8071cd6068dc1cf900c2cf6
Chris Kiefer <chrisk13fer@gmail.com>
2012-10-04 11:11:06 +0100

    DC Blocker

--- ofxMaxim/ofxMaxim/libs/maximilian.h
+++ ofxMaxim/ofxMaxim/libs/maximilian.h
@@ -512,5 +512,17 @@ private:
     double attack, release, env;
 };
 
+//from https://ccrma.stanford.edu/~jos/filters/DC_Blocker_Software_Implementations.html
+class maxiDCBlocker {
+public:
+    double xm1, ym1;
+    maxiDCBlocker() : xm1(0), ym1(0) {}
+    inline double play(double input, double R) {
+        ym1 = input - xm1 + R * ym1;
+        xm1 = input;
+        return ym1;
+    }
+};
+
 
 #endif

8a3e8b38a83de16401bfc3c1c164f447304b8215
mick <mickgrierson@gmail.com>
2012-11-24 00:23:57 +0000

    bandlimited oscs

--- main.cpp
+++ main.cpp
@@ -18,13 +18,13 @@ void setup() {//some inits
 void play(double *output) {
 
 //      no noise here
-//      out=VCO1.sawn(ramp.phasor(0.1,50,8000));
+      out=VCO1.sawn(ramp.phasor(0.1,50,8000));
 
     
 //      Band limited square using two band limited saws. Comment the above and uncomment the below.
 //      you can hear a tiny bit of aliasing approaching 8000 hz but it should do.
 //      It's actually a bit of a weird hack but has nice response.
-        out=VCO1.sawn(ramp.phasor(0.1,50,8000))-VCO2.sawn(ramp2.phasor(0.1,50,8000));
+//        out=VCO1.sawn(ramp.phasor(0.1,50,8000))-VCO2.sawn(ramp2.phasor(0.1,50,8000));
     
 //      When you're done testing with the ramps, see here for an easy PWM hack with this approach
 //      out=VCO1.sawn(100)-(VCO2.sawn(100.1)*0.9);

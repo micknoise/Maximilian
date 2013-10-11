eabb1855777c8451c8a35bae4309ee112ac4460e
Chris Kiefer <chrisk13fer@gmail.com>
2013-10-11 11:13:00 +0100

    additive example

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
 #include "maximilian.h"
 
-maxiOsc mySine;//let's create an oscillator and give it a name.
+const int NUM = 200;
 
+maxiOsc sineBank[NUM];//let's create an oscillator and give it a name.
+maxiOsc modu;
 void setup() {//some inits
 	//nothing to go here this time
 }
 
+
+
+
 void play(double *output) {//this is where the magic happens. Very slow magic.
+    double  wave = modu.saw(100);
+    *output = wave;//simple as that!
 	
-	*output=mySine.sinewave(440);//simple as that!
-	
+    
 }
 

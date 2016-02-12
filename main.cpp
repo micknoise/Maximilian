5a99ccdffc64d09de692c9ab77dc7a21a8fae552
mick grierson <mickgrierson@gmail.com>
2016-02-12 17:42:02 +0000

    adding OF 0.92 granular eexample

--- main.cpp
+++ main.cpp
@@ -1,49 +1,61 @@
-//Using BPF equation from http://www.musicdsp.org/files/Audio-EQ-Cookbook.txt
-
 #include "maximilian.h"
 
-float xs[3], ys[3];
-float a0, a1, a2, b0, b1, b2;
-float f0 = 400; //THE FREQUENCY
-float Q = 1.0;
+//Bizarelly, this sounds a little bit like Kraftwerk's 'Metropolis', although it isn't. Funny that.
 
-maxiOsc mySwitchableOsc;
+maxiOsc sound,bass,timer,mod,lead,lead2,leadmod;//here are the synth bits
+maxiEnv envelope, leadenvelope;//some envelopes
+maxiFilter filter, filter2;//some filters
+maxiDelayline delay;//a delay
+convert mtof;//a method for converting midi notes to frequency
+double bassout,leadout, delayout;//some variables to hold the data and pass it around
+int trigger, trigger2, newnote;//some control variables
+int currentCount,lastCount,playHead=0, currentChord=0;//some other control variables
+int pitch[8]={57,57,59,60};//the bassline for the arpeggio
+int chord[8]={0,0,7,2,5,5,0,0};//the root chords for the arpeggio
+float currentPitch,leadPitch;//the final pitch variables
+
+//here's the lead line trigger array, followed by the pitches
+int leadLineTrigger[256]={1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int leadLinePitch[15]={69,67,65,64,67,66,64,62,65,64,62,57,55,60,57};
+
+
 
 void setup() {//some inits
-    double w0 = 2*PI*f0/44100;
-    double alpha = sin(w0)/(2*Q);
-    //Band-pass reson:
-//    b0 =   alpha;
-//     b1 =   0;
-//     b2 =  -1 * alpha;
-//     a0 =   1 + alpha;
-//     a1 =  -2*cos(w0);
-//     a2 =   1 - alpha;
     
-    //Notch:
-     b0 =   1;
-     b1 =  -2*cos(w0);
-     b2 =   1;
-     a0 =   1 + alpha;
-     a1 =  -2*cos(w0);
-     a2 =   1 - alpha;
-    
-    //LPF:
-//    b0 =  (1 - cos(w0))/2;
-//    b1 =   1 - cos(w0);
-//    b2 =  (1 - cos(w0))/2;
-//    a0 =   1 + alpha;
-//    a1 =  -2*cos(w0);
-//    a2 =   1 - alpha;
 }
 
-void play(double *output) {
-    xs[0] = mySwitchableOsc.sawn(400);
-    ys[0] = (b0/a0)*xs[0] + (b1/a0)*xs[1] + (b2/a0)*xs[2]
-    - (a1/a0)*ys[1] - (a2/a0)*ys[2];
+void play(double *output) {//this is where the magic happens. Very slow magic.
     
-    *output = ys[0];
+    currentCount=(int)timer.phasor(9);//this sets up a metronome that ticks every so often
     
-    ys[2] = ys[1]; ys[1] = ys[0];
-    xs[2] = xs[1]; xs[1] = xs[0];
+    if (lastCount!=currentCount) {//if we have a new timer int this sample, play the sound
+        trigger=1;//play the arpeggiator line
+        trigger2=leadLineTrigger[playHead%256];//play the lead line
+        if (trigger2==1) {//if we are going to play a note
+            leadPitch=mtof.mtof(leadLinePitch[newnote]);//get the next pitch val
+            newnote++;//and iterate
+            if (newnote>14) {
+                newnote=0;//make sure we don't go over the edge of the array
+            }
+        }
+        currentPitch=mtof.mtof(pitch[(playHead%4)]+chord[currentChord%8]);//write the frequency val into currentPitch
+        playHead++;//iterate the playhead
+        if (playHead%32==0) {//wrap every 4 bars
+            currentChord++;//change the chord
+        }
+        //cout << "tick\n";//the clock ticks
+        lastCount=0;//set lastCount to 0
+    }
+    
+    bassout=filter2.lores(envelope.adsr(bass.saw(currentPitch*0.5)+sound.pulse(currentPitch*0.5,mod.phasor(1)),1,0.9995, 0.25, 0.9995, 1, trigger),9250,2);//new, simple ADSR.
+    leadout=filter.lores(leadenvelope.ar(lead2.saw(leadPitch*4)+lead.pulse(leadPitch+(leadmod.sinebuf(1.9)*1.5), 0.6), 0.00005, 0.999975, 50000, trigger2),5900,10);//leadline
+    
+    delayout=(leadout+(delay.dl(leadout, 14000, 0.8)*0.5))/2;//add some delay
+    
+    if(trigger!=0)trigger=0;//set the trigger to off if you want it to trigger immediately next time.
+    
+    
+    output[0]=(bassout)/2;//sum output
+    output[1]=(bassout)/2;
+    
 }
---
+++ openFrameworks/openFrameworksExamples/OSX/granular0_92/addons.make
@@ -0,0 +1,2 @@
+ofxMaxim
+ofxOsc

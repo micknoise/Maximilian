90bcd4c610efbc5e1df76ac969d6a671f06ffe93
Chris Kiefer <chrisk13fer@gmail.com>
2019-03-29 12:28:39 +0000

    basic Xcode project

--- cpp/commandline/main.cpp
+++ cpp/commandline/main.cpp
@@ -1,16 +1,20 @@
 #include "maximilian.h"
 
-//This shows how the fundamental building block of digital audio - the sine wave.
 //
-maxiOsc mySine;//One oscillator - can be called anything. Can be any of the available waveforms.
+maxiOsc osc1, osc2, osc3, osc4, osc5;//One oscillator - can be called anything. Can be any of the available waveforms.
+maxiFilter filt1;
+maxiDistortion dist;
 void setup() {//some inits
     //nothing to go here this time
 }
 
 void play(double *output) {
-    
-    output[0]=mySine.sinewave(440);
-    output[1]=output[0];
+    double ramp = osc4.phasor(0.1) * 20.0;
+    double freq = maxiMap::linexp(osc2.sinewave(ramp + 0.1),-1,1,50,200);
+    double w = osc1.sawn(freq) + osc3.sawn(freq*1.03);
+    w = filt1.lores(w, maxiMap::linexp(osc5.phasor(0.4),0,1,40,4000), 0.9);
+    w = dist.atanDist(w,10);
+    output[0]= output[1] = w;
     
 }
 

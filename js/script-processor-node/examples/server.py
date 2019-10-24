60975a1701f2bb0d1a90744f8d928803579ee02b
Francisco Bernardo <f.bernardo@sussex.ac.uk>
2019-10-24 01:47:22 +0100

    added badges to Maximilian

--- js/script-processor-node/examples/server.py
+++ js/script-processor-node/examples/server.py
@@ -7,9 +7,9 @@ PORT = int(sys.argv[1]);
 class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     pass
 
-Handler.extensions_map['.wasm'] = 'application/wasm'
+Handler.extensions_map['.wasm'] = 'application/wasm';
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+httpd = SocketServer.TCPServer(("", PORT), Handler);
 
-print "serving at port", PORT
-httpd.serve_forever()
+print ("serving at port", PORT);
+httpd.serve_forever();

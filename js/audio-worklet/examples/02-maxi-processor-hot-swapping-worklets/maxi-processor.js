457c641f5d675ed842f7ba0c95b6e3adfe3d7d4f
Francisco Bernardo <f.bernardo@sussex.ac.uk>
2020-03-09 20:27:05 +0000

    refactoring audioworklet examples

--- js/audio-worklet/examples/02-maxi-processor-hot-swapping-worklets/maxi-processor.js
+++ js/audio-worklet/examples/02-maxi-processor-hot-swapping-worklets/maxi-processor.js
@@ -36,13 +36,10 @@ class MaxiProcessor extends AudioWorkletProcessor {
     for (let outputId = 0; outputId < outputsLength; ++outputId) {
       let output = outputs[outputId];
       const channelLenght = output.length;
-
       for (let channelId = 0; channelId < channelLenght; ++channelId) {
-        const gain = parameters.gain;
-        const isConstant = gain.length === 1
         let outputChannel = output[channelId];
         for (let i = 0; i < outputChannel.length; ++i) {
-          const amp = isConstant ? gain[0] : gain[i]
+          const amp = parameters.gain.length === 1 ? parameters.gain[0] : parameters.gain[i]
           outputChannel[i] = (this.mySine.sawn(60) * this.myOtherSine.sinewave(0.4)) * amp;
         }
       }
